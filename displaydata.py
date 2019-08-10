3f6235b03e849044db0b73bdb0bb4d2bc927db75
Nick Francisci <nickfrancisci@gmail.com>
2019-08-10 11:21:25 -0400

    Allow column reordering and fix sort after change to show commas

--- displaydata.py
+++ displaydata.py
@@ -31,14 +31,20 @@ def run(origin, destination, show_columns=SHOW_COLUMNS, sort_by=SORT_BY):
 
   with open(relative_path) as csvfile:
     reader = csv.DictReader(csvfile)
-    table.field_names = reader.fieldnames
+
+    # Use show_columns for order and filtering
+    table.field_names = show_columns
 
     for row in reader:
       if float(row['TA margin']) < 0:
         continue
 
       output_row = []
-      for value in row.values():
+
+      # Use show_columns for order and filtering
+      ordered_row = [row[col_name] for col_name in show_columns]
+
+      for value in ordered_row:
         try:
           formatted_val = '{:,.2f}'.format(float(value))
           output_row.append(formatted_val)
@@ -48,10 +54,15 @@ def run(origin, destination, show_columns=SHOW_COLUMNS, sort_by=SORT_BY):
       table.add_row(output_row)
 
     table.align = 'r'
+
+    # Not sure why, but there's an off by one error on this index
+    sort_column_index = show_columns.index(sort_by) + 1
+
     return(
       table.get_string(
         fields=show_columns,
         sortby=sort_by,
+        sort_key=lambda row: float(row[sort_column_index].replace(',','')),
         reversesort=True,
       )
     )
